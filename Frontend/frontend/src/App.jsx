c1a36e2f08f2530b900ff15a1935ce72f9e525ab
Shivtej-Pawar <shivtejapawar5@gmail.com>
2025-12-25 23:15:23 +0530

    Add home and register pages

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes,Route } from "react-router"
 import Home from "./pages/Home"
+import Register from "./pages/Register"
+import { ToastContainer } from "react-toastify"
 function App() {
 
   return (
     <>
        <Routes>
           <Route path="/*" element={<Home/>}/>
+          <Route path="/register" element={<Register/>}/>
        </Routes>
+       <ToastContainer />
     </>
   )
 }
